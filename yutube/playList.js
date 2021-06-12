cb244c0b4fe0d84efc21f6d4d9fe3c746c41ca29
a31625802 <83102083+a31625802@users.noreply.github.com>
2021-06-12 18:59:28 +0800

    Add files via upload

---
+++ yutube/playList.js
@@ -0,0 +1,13 @@
+//YouTube Video ID
+let playList = [
+    "JmzIOBbOmEo",
+    "uFbEbMNycMc",
+    "CSjRJZoTmo0"
+    
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [101,109],
+    [144,150],
+    [146,152]
+    ];
